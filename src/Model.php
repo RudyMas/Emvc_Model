06309f702b07f3d0cb83b81436aacf7c53782403
Rudy Mas <rudy.mas@rudymas.be>
2018-09-20 13:58:19 +0200

    Model data is now stored in the Model Class

--- src/Model.php
+++ src/Model.php
@@ -8,17 +8,28 @@ namespace EasyMVC\Model;
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     0.0.1.1
- * @package     EasyMVC\Repository
+ * @version     0.1.0.2
+ * @package     EasyMVC\Model
  */
 class Model
 {
+    private $data;
+
     /**
-     * @param string $field
+     * @param string $key
+     * @param $value
+     */
+    public function setData(string $key, $value): void
+    {
+        $this->data[$key] = $value;
+    }
+
+    /**
+     * @param string $key
      * @return mixed
      */
-    public function get(string $field)
+    public function getData(string $key)
     {
-        return $this->$field;
+        return $this->data[$key];
     }
 }
