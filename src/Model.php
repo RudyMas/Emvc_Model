29c987c7bad9ea7db0f048035927df260f3eb47b
Rudy Mas <rudy.mas@rudymas.be>
2020-05-09 13:18:01 +0200

    Updated to PHP 7.2

--- src/Model.php
+++ src/Model.php
@@ -3,13 +3,13 @@
 namespace EasyMVC;
 
 /**
- * Class Repository
+ * Class Model (PHP version 7.2)
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
  * @copyright   2018-2020, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.0.0.4
- * @package     EasyMVC\Model
+ * @version     1.0.1.0
+ * @package     EasyMVC
  */
 class Model
 {
