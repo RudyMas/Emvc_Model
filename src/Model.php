8abc9a43eaf9335ab6dd246cc73370277fe7b1b7
Rudy Mas <rudy.mas@rudymas.be>
2022-04-06 22:27:42 +0200

    Added function getAll() Updated to PHP 7.4

--- src/Model.php
+++ src/Model.php
@@ -3,12 +3,12 @@
 namespace EasyMVC;
 
 /**
- * Class Model (PHP version 7.2)
+ * Class Model (PHP version 7.4)
  *
  * @author      Rudy Mas <rudy.mas@rmsoft.be>
- * @copyright   2018-2020, rmsoft.be. (http://www.rmsoft.be/)
+ * @copyright   2018-2022, rmsoft.be. (http://www.rmsoft.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.0.1.0
+ * @version     1.1.0.0
  * @package     EasyMVC
  */
 class Model
@@ -42,5 +42,15 @@ class Model
     public function getData(string $key)
     {
         return $this->data[$key];
+    }
+
+    /**
+     * Get all the data
+     *
+     * @return mixed
+     */
+    public function getAll()
+    {
+        return $this->data;
     }
 }
